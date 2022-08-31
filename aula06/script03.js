6345b467e73efbdbd94d8c54ea80b368d8622aea
Marcos Medeiros <marcosbourdon@hotmail.com>
2022-08-31 19:56:44 -0300

    Organização de arquivos

---
+++ aula06/script03.js
@@ -0,0 +1,4 @@
+var n1 = Number(window.prompt('Write the first number:')); //Number.parseInt()
+var n2 = Number(window.prompt('Write the second number:')); //Number.parseFloat()
+var s = n1 + n2;
+window.alert(`The sum of ${n1} and ${n2} is equals to: ${s}`); //Template String
