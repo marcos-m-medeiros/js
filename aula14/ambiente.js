5ce4585ece074cc4d70bd894ef432eefe64957bc
Marcos Medeiros <marcosbourdon@hotmail.com>
2022-08-31 20:31:58 -0300

    Aula 14 - Repetições (Pt. 2)
    for

---
+++ aula14/ambiente.js
@@ -0,0 +1,22 @@
+/*
+//while
+var c = 1;
+while (c <= 5) {
+    console.log(`Contando: ${c}`);
+    c++;
+}
+
+//do..while
+var c = 1;
+do {
+    console.log(`Contando:${c}`);
+    c++;
+} while (c <= 5)
+*/
+
+console.log('Vai começar...')
+//for
+for(var c = 1; c <= 5; c++) {
+    console.log(`Contando: ${c}`);
+}
+console.log('Fim.');
