2a33a0e5fc6b152184c14761310806178c39251b
Marcos Medeiros <marcosbourdon@hotmail.com>
2022-09-08 09:09:00 -0300

    Exercício JavaScript (Pt. 6)

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -1,16 +1,32 @@
 function contar() {
-    var inicio = window.document.getElementById('inicio');
-    var fim = window.document.getElementById('fim');
-    var passo = window.document.getElementById('passo');
-    var contagem = window.document.getElementById('contagem');
+    let inicio = document.querySelector('input#txti')
+    let fim = document.querySelector('input#txtf')
+    let passo = document.querySelector('input#txtp')
+    let resultado = document.querySelector('div#resultado')
 
-    var inicio = Number(inicio.value);
-    var fim = Number(fim.value);
-    var passo = Number(passo.value);
-
-    while (inicio <= fim) {
-        contagem.innerHTML = `<br>Contando: <br>${inicio}`
-        inicio = inicio + passo;
+    if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        resultado.innerHTML = `Impossível contar!`
+        alert('[ERRO] Insira as informações corretamente')
+    } else {
+        resultado.innerHTML = `Contando: <br> `
+        let i = Number(inicio.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if (p <= 0) {
+            alert('Passo inválido! Considerando PASSO 1')
+            p = 1
+        }
+        if (i < f) {
+            //Contagem crescente
+            for (let c = i; c <= f; c += p) {
+                resultado.innerHTML += ` ${c} \u{1F449}`
+            }
+        } else {
+            //Contagem regressiva
+            for (let c = i; c >= f; c -= p) {
+                resultado.innerHTML += ` ${c} \u{1F449}`
+            }
+        }
+        resultado.innerHTML += `\u{1F3C1}`
     }
 }
-
