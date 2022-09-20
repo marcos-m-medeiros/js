3a29c8d5eee1ccc3d577c582e08bd842311d366a
Marcos Medeiros <marcosbourdon@hotmail.com>
2022-09-20 18:04:16 -0300

    final commit

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -1,16 +1,23 @@
 function contar() {
-    var inicio = window.document.getElementById('inicio');
-    var fim = window.document.getElementById('fim');
-    var passo = window.document.getElementById('passo');
-    var contagem = window.document.getElementById('contagem');
+    let ini = document.querySelector('input#txti')
+    let fim = document.querySelector('input#txtf')
+    let passo = document.querySelector('input#txtp')
+    let res = document.querySelector('div#resultado')
 
-    var inicio = Number(inicio.value);
-    var fim = Number(fim.value);
-    var passo = Number(passo.value);
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        alert('[ERRO] Faltam dados!')
+    } else {
+        res.innerHTML = 'Contando...'
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if (i < f) {
+            for (let c = 1; c <= f; c += p) {
+                res.innerHTML += ` ${c} \u{1F449}`
+            }
+            res.innerHTML += `\u{1F3C1}`
+        } else {
 
-    while (inicio <= fim) {
-        contagem.innerHTML = `<br>Contando: <br>${inicio}`
-        inicio = inicio + passo;
+        }
     }
 }
-
